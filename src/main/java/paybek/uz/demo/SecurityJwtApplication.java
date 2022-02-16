7c8e24604ad6d73273007bfe4b7d3f87bb3b696f
Sanjarbekk <Sanjar7175>
2022-02-16 11:01:18 +0500

    Closed #3 added swagger

--- src/main/java/paybek/uz/demo/SecurityJwtApplication.java
+++ src/main/java/paybek/uz/demo/SecurityJwtApplication.java
@@ -2,8 +2,11 @@ package paybek.uz.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
 public class SecurityJwtApplication {
 
     public static void main(String[] args) {
